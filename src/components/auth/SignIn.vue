9278126199fb36f4274f3cdec9112a0bb107804a
vlad <caraseli02@gmail.com>
2022-09-11 12:08:43 +0200

    feat: add louding ui

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -21,6 +21,8 @@ interface FireAuthError {
   [key: string]: string
 }
 
+const router = useRouter()
+
 const alerts = useAlertsStore();
 
 const authStore = useAuthStore()
@@ -48,12 +50,14 @@ const form: Form = $ref({
   password: '',
 })
 const showForgotPopUp = $ref(false)
-const loading = $ref(true)
+let loading = $ref(false)
 const signInWithEmailAndPassword = async (form: Form) => {
 
 try {
+  loading = true
   await authStore.signInWithEmailAndPassword(form.email, form.password)
-  useRouter().push("/");
+  loading = false
+  router.push({ name: 'Home' });
 } catch (err: any) {
   const testData: FireAuthError = data
   alerts.authError(testData[err.code]);
@@ -84,16 +88,18 @@ try {
             type="password"
             rules="required"
           />
-          <AppSpinner v-if="loading" color="red" size="sm" class="ml-2 mt-2" />
-        
-          <PrimaryBtn v-else class="mt-4">
+          <PrimaryBtn class="mt-4">
             <button
               id="loginBtn"
               type="submit"
               class="w-40 flex justify-center"
             >
               Entrar
-              <ArrowCircleRightIcon class="h-5 w-5 ml-4" />
+              <svg v-if="loading" class="animate-spin ml-4 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+              <ArrowCircleRightIcon v-else class="h-5 w-5 ml-4" />
             </button>
           </PrimaryBtn>
         </VeeForm>
